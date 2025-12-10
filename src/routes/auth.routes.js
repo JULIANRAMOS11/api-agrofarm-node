825909ebea7e4cc8b0434833c76d54298811b227
julianramos11 <1500ramosjulian@gmail.com>
2025-12-09 22:24:39 -0500

    Agregar testing con Postman y modulo de cerdos (EV04)

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,13 +1,15 @@
 // src/routes/auth.routes.js
+// Rutas de autenticación AGROFARM
+
 import { Router } from "express";
 import { register, login } from "../controllers/auth.controller.js";
 
 const router = Router();
 
-// Ruta de registro
+// Ruta final: POST http://localhost:4000/api/auth/register
 router.post("/register", register);
 
-// Ruta de login
+// Ruta final: POST http://localhost:4000/api/auth/login
 router.post("/login", login);
 
 export default router;
